ba8593c3ef8c9fd1b8aaf7b88c9128b9b99d5591
Marcel Bruse <marcel.bruse@posteo.de>
2019-12-29 11:26:29 +0100

    The editor view model will be created in the editor activity. The editor view will be created in the editor activity. Editor activity listens to the view model in order to close itself upon successful saving of the quota.

--- app/src/main/java/de/quotas/activities/editor/EditorActivity.kt
+++ app/src/main/java/de/quotas/activities/editor/EditorActivity.kt
@@ -1,31 +1,57 @@
 package de.quotas.activities.editor
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.google.android.material.textfield.TextInputEditText
 import de.quotas.QuotasApplication
 import de.quotas.R
 import de.quotas.activities.ActivityArgumentKeys.QUOTA_ID
 
 class EditorActivity : AppCompatActivity() {
 
+    private lateinit var editorViewModel: EditorViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.editor_activity)
         setSupportActionBar(findViewById(R.id.editor_toolbar))
         val quotaId = intent.getLongExtra(QUOTA_ID, -1L)
+        editorViewModel = createEditorViewModel(quotaId)
+        createView(editorViewModel)
+    }
 
+    private fun createEditorViewModel(quotaId: Long): EditorViewModel {
         val quotasApplication = application as QuotasApplication
         val quotasRepository = quotasApplication.getModelComponent().getQuotasRepository()
-        val editorViewModel = EditorViewModel(quotasRepository)
-        val quota = editorViewModel.getQuota(quotaId)
-        Log.d("qname", "Quota: $quota")
+        return if (quotaId > -1) {
+            EditorViewModel(quotasRepository, quotaId)
+        } else {
+            EditorViewModel(quotasRepository)
+        }
+    }
+
+    private fun createView(editorViewModel: EditorViewModel) {
+        val quotaNameField = findViewById<TextInputEditText>(R.id.name_field)
+        quotaNameField.setText(editorViewModel.getQuotaName())
+        editorViewModel.quotaSaved.observe(this, Observer { finish() })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.editor_menu, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.save_menu_item -> {
+                editorViewModel.saveQuota()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
--- app/src/main/java/de/quotas/activities/editor/EditorViewModel.kt
+++ app/src/main/java/de/quotas/activities/editor/EditorViewModel.kt
@@ -1,11 +1,22 @@
 package de.quotas.activities.editor
 
 import androidx.lifecycle.ViewModel
+import de.quotas.google.SingleLiveEvent
 import de.quotas.models.Quota
 import de.quotas.models.QuotaRepository
 
-class EditorViewModel(private val quotaRepository: QuotaRepository) : ViewModel() {
+class EditorViewModel(private val quotaRepository: QuotaRepository, private val quotaId: Long) : ViewModel() {
 
-    fun getQuota(quotaId: Long): Quota? = quotaRepository.getQuota(quotaId).value
+    val quotaSaved = SingleLiveEvent<Any>()
+
+    constructor(quotaRepository: QuotaRepository): this(quotaRepository, -1L)
+
+    private fun getQuota(): Quota? = quotaRepository.getQuota(quotaId).value
+
+    fun getQuotaName() = getQuota()?.name ?: ""
+
+    fun saveQuota() {
+        quotaSaved.call()
+    }
 
 }
